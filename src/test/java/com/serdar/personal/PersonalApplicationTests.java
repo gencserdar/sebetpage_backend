9e8bcdb0e0ff20a52a3462bb11a5c5a9c407911a
serdar genç. <gencserdar3437@gmail.com>
2025-07-29 10:57:13 +0300

    first commit

---
+++ src/test/java/com/serdar/personal/PersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serdar.personal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
